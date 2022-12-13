33d333a85c879ab50e58d3c281cba6c0b122cd49
Rune Nielsen <runenielsen@runbox.com>
2022-12-13 21:16:19 +0100

    implements totp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-mod hotp;
+mod otp;
 
 fn main() {}
--- src/otp.rs
+++ src/otp.rs
@@ -1,18 +1,25 @@
+use std::time::SystemTime;
+
 use hmac::{Hmac, Mac};
 use sha1::Sha1;
 
-type HmacSha1 = Hmac<Sha1>;
+fn step_counter(time: &SystemTime, step: u64) -> u64 {
+    time.duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+        / step
+}
 
 /// HMAC-based one-time password for a given secret and counter.
 fn hotp(secret: &str, counter: u64) -> u32 {
-    let digest = HmacSha1::new_from_slice(&secret.to_owned().into_bytes())
+    let digest = Hmac::<Sha1>::new_from_slice(&secret.to_owned().into_bytes())
         .unwrap()
         .chain_update(counter.to_be_bytes())
         .finalize()
         .into_bytes();
 
     let offset = (digest[19] & 0xf) as usize;
-    let code: u32 = u32::from(digest[offset] & 0x7f) << 24
+    let code = u32::from(digest[offset] & 0x7f) << 24
         | u32::from(digest[(offset + 1)]) << 16
         | u32::from(digest[(offset + 2)]) << 8
         | u32::from(digest[(offset + 3)]);
@@ -20,12 +27,24 @@ fn hotp(secret: &str, counter: u64) -> u32 {
     code % 1_000_000
 }
 
+pub fn totp(secret: &str, interval: u64) -> u32 {
+    hotp(secret, step_counter(&SystemTime::now(), interval))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn get_hotp_no_counter() {
+    fn get_totp() {
+        // Want make sure that something is returned.
+        // We cannot, assert on specific values, since the current time
+        // changes all the time.
+        assert!(totp("hello", 30) > 0);
+    }
+
+    #[test]
+    fn get_hotp() {
         let assertions = [
             (124_111, "Hello world!"),
             (654_079, "it works!"),

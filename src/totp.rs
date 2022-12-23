981b5fff2698511e7fda39a9bb7f3c65402883bd
Rune Nielsen <runenielsen@runbox.com>
2022-12-23 14:07:58 +0100

    now tests that the totp code is correct

--- src/totp.rs
+++ src/totp.rs
@@ -50,3 +50,32 @@ impl Totp {
             % self.interval
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use super::*;
+
+    #[test]
+    fn totp_code_calculation_is_correct() {
+        let march_14_2020 = SystemTime::UNIX_EPOCH + Duration::new(1_584_188_800, 0);
+
+        let test_data = [
+            (640_572, Totp::new("Acme Inc.", "8n4mzt7w", 6, 30)),
+            (87439, Totp::new("Gizmo Corporation", "xkc2j8fh", 6, 30)),
+            (771_990, Totp::new("Foo Industries", "9s6bk3jq", 6, 30)),
+            (438_943, Totp::new("Bar Enterprises", "7h1lm5rp", 6, 30)),
+            (52859, Totp::new("Baz Inc.", "2v9d4k8c", 6, 30)),
+            (669_413, Totp::new("Qux Limited", "5j6w7m2v", 6, 30)),
+            (203_698, Totp::new("Quux Corp.", "3p8s1q9z", 6, 30)),
+            (640_828, Totp::new("Corge Enterprises", "4y7e2u5k", 6, 30)),
+            (619_356, Totp::new("Grault Inc.", "6f9h2l5m", 6, 30)),
+            (73510, Totp::new("Garply Co.", "1d4t7h2v", 6, 30)),
+        ];
+
+        for (expected, totp) in test_data {
+            assert_eq!(expected, totp.code(&march_14_2020));
+        }
+    }
+}

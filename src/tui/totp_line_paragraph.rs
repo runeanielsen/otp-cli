82477ba21e038802af735148704841e6f6977cfd
runeanielsen <runenielsen@runbox.com>
2023-02-04 08:40:19 +0100

    only redraw totp paragraph line if the paragraph text has changed

--- src/tui/totp_line_paragraph.rs
+++ src/tui/totp_line_paragraph.rs
@@ -11,12 +11,27 @@ use super::{widgets::LineParagraph, Display, Element, HandleEvent, Refresh};
 
 pub struct TotpLineParagraph {
     line_paragraph: LineParagraph,
+    redraw: bool,
 }
 
 impl TotpLineParagraph {
     pub fn new() -> Self {
         Self {
             line_paragraph: LineParagraph::new(String::new()),
+            // It's important to set the redraw to true first time we run,
+            // otherwise it will first be redrawn on next change.
+            redraw: true,
+        }
+    }
+
+    fn update_line_text(&mut self, duration_max: u64, time: SystemTime) {
+        let duration_used = totp::duration_used(duration_max, time);
+        let new_paragraph_text = format!("{duration_used:02}/{duration_max}");
+
+        // No reason to redraw if the text has not changed.
+        if new_paragraph_text != self.line_paragraph.text {
+            self.line_paragraph.text = new_paragraph_text;
+            self.redraw = true;
         }
     }
 }
@@ -25,11 +40,16 @@ impl Element for TotpLineParagraph {}
 
 impl Display for TotpLineParagraph {
     fn display(&mut self, w: &mut Stdout) -> Result<(), Box<dyn Error>> {
-        queue!(
-            w,
-            style::PrintStyledContent(self.line_paragraph.text.clone().grey()),
-            cursor::MoveToNextLine(1)
-        )?;
+        if self.redraw {
+            queue!(
+                w,
+                style::PrintStyledContent(self.line_paragraph.text.clone().grey()),
+            )?;
+
+            self.redraw = false;
+        }
+
+        queue!(w, cursor::MoveToNextLine(1))?;
 
         Ok(())
     }
@@ -37,9 +57,7 @@ impl Display for TotpLineParagraph {
 
 impl Refresh for TotpLineParagraph {
     fn refresh(&mut self) {
-        let interval = 30;
-        let duration_used = totp::duration_used(interval, SystemTime::now());
-        self.line_paragraph.text = format!("{duration_used:02}/{interval}");
+        self.update_line_text(30, SystemTime::now());
     }
 }
 

685c3967e7a071d1b82181de517b899105d4f59b
runeanielsen <runenielsen@runbox.com>
2023-03-13 22:04:11 +0100

    make sure that clipboard can be accessed bofore loading configs

--- src/main.rs
+++ src/main.rs
@@ -26,17 +26,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     // and this project only supports UNIX based systems.
     #[allow(deprecated)]
     let home_dir = env::home_dir().expect("Could not load user home directory.");
-    let default_config_path = [home_dir, PathBuf::from(".config/totp-tui")]
-        .iter()
-        .collect();
-
-    let mut stdout = stdout();
 
     let clipboard = Arc::new(Mutex::new(
         Clipboard::new().expect("Could not get access to the clipboard."),
     ));
 
+    let default_config_path = [home_dir, PathBuf::from(".config/totp-tui")]
+        .iter()
+        .collect();
+
     let totps: Vec<Totp> = config::load_totps(default_config_path, DIGITS, INTERVAL)?;
+
+    let mut stdout = stdout();
 
     tui::start(
         &mut stdout,
